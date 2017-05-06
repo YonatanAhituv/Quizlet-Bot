40aaaa8666f595c24b11002ba1ce257eebbafb90
AtomicCoding <atomicgaming4life@gmail.com>
2017-05-06 10:33:06 -0700

    Minor Update

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import os
 import time
 loggedIn = False
 oneQuiz = False
+timesSkipped = 0
+dominationTold = False
 with open('email.txt', 'r') as myfile:
     email=myfile.read().replace('\n', '')
 with open('password.txt', 'r') as myfile:
@@ -38,6 +40,11 @@ def login():
 pageID = (Website ID)
 if oneQuiz == False:
     while True:
+        if timesSkipped > 80:
+            browser.quit()
+            if dominationTold == False:
+                print("Quizlet Domination Complete.")
+                dominationTold = True
         try:
             browser.get("https://quizlet.com/"+str(pageID)+"/micromatch")
             browser.find_element_by_id("start").click()
