797447ab470d824d54acf02cf4bd6201c4d14a30
AtomicCoding <atomicgaming4life@gmail.com>
2017-05-05 19:11:23 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Welcome!
+# Prerequisites
+Python (Obviously)
+Selenium
+Chrome Driver
+# Installation
+Create two new text files in the directory the script is stored in, one called: password.txt and another called email.txt, then inside of password.txt, enter the password to the Google Account linked with your Quizlet Account, then inside of email.txt, enter the email to the Google Account linked with your Quizlet Account. Then, open up the script and on line 17, where it says: "chromedriver = "(Insert ChromeDriver path here)", replace the "(Insert ChromeDriver path here)" with your ChromeDriver path, then run the script and you should be fine!
+# Is that it?
+Yes...that is it.
---
+++ main.py
@@ -0,0 +1,63 @@
+from selenium import webdriver
+import os
+import time
+from selenium.webdriver.common.keys import Keys
+loggedIn = False
+oneQuiz = False
+with open('email.txt', 'r') as myfile:
+    email=myfile.read().replace('\n', '')
+with open('password.txt', 'r') as myfile:
+    password=myfile.read().replace('\n', '')
+print("Type in: 1, to do just one quizlet, type in 2, to do multiple")
+runTypeInput = input("I choose: #")
+if runTypeInput == "1":
+    oneQuiz = True
+if runTypeInput == "2":
+    oneQuiz = False
+chromedriver = "(Insert ChromeDriver path here)"
+os.environ["webdriver.chrome.driver"] = chromedriver
+browser = webdriver.Chrome(chromedriver)
+browser.set_window_size(1600, 1000)
+def click(id):
+    try:
+        browser.find_element_by_id("definition-"+id).click()
+        browser.find_element_by_id("term-"+id).click()
+    except:
+        pass
+def login():
+    try:
+        browser.find_element_by_xpath("//div[@class='TrophiesModal-loggedOutActions']/div/button").click()
+    except:
+        browser.find_element_by_xpath("//a[@href][2]").click()
+    time.sleep(2)
+    browser.find_element_by_xpath("//div[@class='UISocialButton']/a").click()
+    browser.find_element_by_xpath("//input[@type='email']").send_keys(email+Keys.ENTER)
+    time.sleep(1)
+    browser.find_element_by_xpath("//input[@type='password']").send_keys(password+Keys.ENTER)
+
+pageID = 1000
+if oneQuiz == False:
+    while True:
+        browser.get("https://quizlet.com/"+str(pageID)+"/micromatch")
+        browser.find_element_by_id("start").click()
+        terms = browser.find_elements_by_xpath("//a[@data-type='term']")
+        for term in terms:
+            click(term.get_attribute("data-id"))
+        time.sleep(1)
+        if not loggedIn:
+            login()
+            loggedIn = True
+        pageID = pageID + 1
+        time.sleep(2)
+if oneQuiz == True:
+    while True:
+        browser.get("https://quizlet.com/"+str(pageID)+"/micromatch")
+        browser.find_element_by_id("start").click()
+        terms = browser.find_elements_by_xpath("//a[@data-type='term']")
+        for term in terms:
+            click(term.get_attribute("data-id"))
+        time.sleep(1)
+        if not loggedIn:
+            login()
+            loggedIn = True
+        time.sleep(2)
