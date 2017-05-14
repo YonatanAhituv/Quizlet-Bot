c61dafa99d7b3bb1aa219f0ceed1707687c33490
AtomicCoding <atomicgaming4life@gmail.com>
2017-05-14 14:31:06 -0700

    Bug Fixes and Additions

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ loggedIn = False
 oneQuiz = False
 osSelected = False
 noJSON = False
+pageIDChoosen = False
 bulletMaker = 0
 osis = 0
 # 0 = Mac, 1 = Windows, 2 = Linux
@@ -76,9 +77,9 @@ if path == "ns":
 if (path == "ns"):
     while (checkedforchrome == False):
         checkedforchrome = True
-        chromecheck = input('Have you installed ChromeDriver (Y or N)? ')
+        chromecheck = input('Have you installed ChromeDriver (Y or N)? >>> ')
         if (chromecheck == "y" or chromecheck == "Y"):
-            path = input("The path to chromedriver is: ")
+            path = input("The path to chromedriver is: >>> ")
             if not os.path.exists(path):
                 print("Invalid Path!")
                 checkedforchrome = False
@@ -91,11 +92,11 @@ if (path == "ns"):
             checkedforchrome = False
 
         if (chromecheck == "n" or chromecheck == "N"):
-            chromeinstalled = input("Would you like the script to install it for you (Y or N)? ")
+            chromeinstalled = input("Would you like the script to install it for you (Y or N)? >>> ")
             if (chromeinstalled == "y" or chromeinstalled == "Y"):
                 while (osSelected == False):
                     osSelected = True
-                    useros = input("What OS are you on (W for Windows, M for Mac, L64 for 64 bit Linux, and L32 for 32 bit Linux)? ")
+                    useros = input("What OS are you on (W for Windows, M for Mac, L64 for 64 bit Linux, and L32 for 32 bit Linux)? >>> ")
                     print("Downloading...")
                     if (useros == "w" or useros == "W"):
                         downloadurl = "https://chromedriver.storage.googleapis.com/2.29/chromedriver_win32.zip"
@@ -122,39 +123,115 @@ if (path == "ns"):
                     print("Goodbye!")
                     sys.exit()
 if (username == "ns" and password == "ns"):
-        reply = input('Would you like to have the script enter your username and password for you (Y or N)? ')
+        reply = input('Would you like to have the script enter your username and password for you (Y or N)? >>> ')
         if (reply == "n" or reply == "N"):
             username = "dw"
             password = "dw"
         if (reply == "Y" or reply == "y"):
             print("None of this data is transmitted, it is just saved for ease of use on your local machine.")
-            username = input("Email: ")
-            password = getpass.getpass("Password: ")
+            username = input("Email: >>> ")
+            password = getpass.getpass("Password: >>> ")
         save(info, pageID, successes, failures, path, username, password)
         print("Thank you!")
 print("INFO: Only close the browser, not the script or terminal")
 runTypeSelected = False
 while runTypeSelected == False:
     runTypeSelected = True
-    print("Type in an option: Start, Read Data, Reset Data, Quit")
+    print("Type in an option: Start, Settings, Read Data, Reset Data, Quit")
     time.sleep(0.1)
-    runTypeInput = input("I choose: ")
+    runTypeInput = input("I choose: >>> ")
     time.sleep(0.1)
     runTypeInput = runTypeInput.upper()
     if runTypeInput == "START":
-        chooseRunType = input("Would you like to do multiple quizes (Y or N)? ")
+        chooseRunType = input("Would you like to do multiple quizes (Y or N)? >>> ")
         if (chooseRunType == "y" or chooseRunType == "Y"):
             if pageID == "ns":
                 print("https://quizlet.com/0<---PageID/micromatch")
-                pageID = input("What pageID would you like to start from (Please type in an integer)? ")
-                save(info, pageID, successes, failures, path, username, password)
+                while pageIDChoosen == False:
+                    pageIDChoosen = True
+                    pageID = input("What pageID would you like to start from (Please type in an integer)? >>> ")
+                    try:
+                        pageID = int(pageID)
+                        save(info, pageID, successes, failures, path, username, password)
+                    except ValueError:
+                        pageIDChoosen = False
             oneQuiz = False
         if (chooseRunType == "n" or chooseRunType == "N"):
             if pageID == "ns":
                 print("https://quizlet.com/0<---PageID/micromatch")
-                pageID = input("What pageID would you like the bot to run on (Please type in an integer)? ")
-                save(info, pageID, successes, failures, path, username, password)
-            oneQuiz = True
+                pageIDChoosen = False
+                while pageIDChoosen == False:
+                    pageIDChoosen = True
+                    pageID = input("What pageID would you like the bot to run on (Please type in an integer)? >>> ")
+                    try:
+                        pageID = int(pageID)
+                        save(info, pageID, successes, failures, path, username, password)
+                    except ValueError:
+                        pageIDChoosen = False
+                oneQuiz = True
+    if runTypeInput == "SETTINGS":
+        doneChanging = False
+        while doneChanging == False:
+            save(info, pageID, successes, failures, path, username, password)
+            whattochange = input("Type P to change the PageID, Type PA to change the path to ChromeDriver, Type E to change the email, Type PAS to change the password, or type Q to exit. >>> ")
+            whattochange = whattochange.upper()
+            if whattochange == "P":
+                if pageID == "ns":
+                    print("PageID has not been set.")
+                else:
+                    print("PageID is set to:", pageID)
+                pageIDChoosen = False
+                while pageIDChoosen == False:
+                    pageIDChoosen = True
+                    pageID = input("What would you like to set the pageID to? >>> ")
+                    try:
+                        pageID = int(pageID)
+                        save(info, pageID, successes, failures, path, username, password)
+                    except ValueError:
+                        pageIDChoosen = False
+                doneChanging = False
+            if whattochange == "PA":
+                if path == "ns":
+                    print("The path to ChromeDriver has not been set.")
+                else:
+                    print("The path to ChromeDriver is set to:", path)
+                    checkedforchrome = False
+                    while (checkedforchrome == False):
+                            checkedforchrome = True
+                            path = input("I would like to set the path to ChromeDriver to: >>> ")
+                            if not os.path.exists(path):
+                                print("Invalid Path!")
+                                checkedforchrome = False
+                    doneChanging = False
+            if whattochange == "E":
+                if username == "ns":
+                    print("The email has not been set.")
+                if username == "nw":
+                    print("You have disabled automatic password and email entering.")
+                if not username == "ns" and not username == "nw":
+                    print("The email is set to:", username)
+                if username == "nw":
+                    enableemailandpassword = input("Would you like to enable email and password entering (Y or N)? >>> ")
+                    enableemailandpassword = enableemailandpassword.upper()
+                if not username == "nw":
+                    enableemailandpassword = "Y"
+                if enableemailandpassword == "Y" or not email == "dw":
+                    email = input("I would like to set the email to: >>> ")
+                doneChanging = False
+            if whattochange == "PAS":
+                verifypassword = getpass.getpass("Please enter your password to continue: >>> ")
+                if verifypassword == password:
+                    print("Correct!")
+                    password = getpass.getpass("I would like to change my password to: >>> ")
+                else:
+                    print("Invalid Password!")
+                doneChanging = False
+            if whattochange == "Q":
+                print("Leaving...")
+                doneChanging = True
+                runTypeSelected = False
+            if not whattochange == "P" and not whattochange == "PA" and not whattochange == "E" and not whattochange == "PAS" and not whattochange == "Q":
+                doneChanging = False
     if runTypeInput == "READ DATA":
         if pageID == "ns":
             print("PageID has not been set.")
@@ -183,8 +260,10 @@ while runTypeSelected == False:
                 bulletMaker = bulletMaker + 1
             bulletMaker = 0
             print("The password is:", passwordhidden)
+            sys.stdout.flush()
             passwordprotect = getpass.getpass("Enter the password to unhide the password: ")
             if (passwordprotect == password):
+                sys.stdout.flush()
                 print("The password is:", password)
             else:
                 print("Incorrect!")
@@ -201,7 +280,7 @@ while runTypeSelected == False:
     if runTypeInput == "QUIT":
         print("Goodbye!")
         sys.exit()
-    if not runTypeInput == "START" and not runTypeInput == "RESET DATA" and not runTypeInput == "QUIT" and not runTypeInput == "READ DATA":
+    if not runTypeInput == "START" and not runTypeInput == "RESET DATA" and not runTypeInput == "QUIT" and not runTypeInput == "READ DATA" and not runTypeInput == "SETTINGS":
         runTypeSelected = False
 chromedriver = path
 os.environ["webdriver.chrome.driver"] = chromedriver
